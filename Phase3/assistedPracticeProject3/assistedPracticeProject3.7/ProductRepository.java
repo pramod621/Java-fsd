5d48beaa7e42b476eeebe8820cc70c13762c71d3
Pramod <80201143+pramod621@users.noreply.github.com>
2023-11-02 16:17:24 +0530

    Add files via upload

---
+++ Phase3/assistedPracticeProject3/assistedPracticeProject3.7/ProductRepository.java
@@ -0,0 +1,7 @@
+package coms.phase3AssistedPracticeProject37;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer>{
+
+}
+
